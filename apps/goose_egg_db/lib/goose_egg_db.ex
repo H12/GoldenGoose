b3e188b9b343eeb037a401b95a195d041dbf6a7b
H12 <henrynf12@gmail.com>
2020-01-19 18:47:56 -0600

    Add games & plays helpers w/ field attrs

--- apps/goose_egg_db/lib/goose_egg_db.ex
+++ apps/goose_egg_db/lib/goose_egg_db.ex
@@ -6,4 +6,31 @@ defmodule GooseEggDb do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  import MLBStats.Client, only: [ranged_schedule: 3, game_play_by_play: 2]
+
+  @play_by_play_fields [
+    :playsByInning,
+    :top,
+    :bottom,
+    :allPlays,
+    :matchup,
+    :batter,
+    :pitcher,
+    :id,
+    :fullName,
+    :result,
+    :awayScore,
+    :homeScore
+  ]
+
+  @schedule_fields [:dates, :games, :gamePk, :gameType]
+
+  def games_for_dates(start_date, end_date) do
+    ranged_schedule(start_date, end_date, @schedule_fields)
+  end
+
+  def plays_for_game(gamePk) do
+    game_play_by_play(gamePk, @play_by_play_fields)
+  end
 end
