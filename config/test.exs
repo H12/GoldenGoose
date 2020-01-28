cb54845592c0398205e9176efb3fecef78734173
Henry Firth <henrynf12@gmail.com>
2020-01-27 20:04:32 -0600

    Add Elixir CI action workflow

--- config/test.exs
+++ config/test.exs
@@ -8,4 +8,11 @@ config :goose_egg_db, GooseEggDb.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :goose_egg_db, GooseEggDb.Repo,
+    username: "postgres",
+    password: "postgres"
+end
+
 config :tesla, adapter: Tesla.Mock
